6f316cdb90d2c8141f483ad32581dc83309e1281
Justin Turner <mrfathed@protonmail.com>
2021-12-05 07:25:34 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    number = int(input())
+    if number >= 0:
+        print(number)
+    else:
+        print(number * -1)
 
 if __name__ == '__main__':
     main()
